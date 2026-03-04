a91417ff522bc57b956b7c281d0e644fb73a1748
dipanshu <dipanshu.swiss.dreams@gmail.com>
2026-03-05 01:09:36 +0530

    changes

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world hiii');
+res.send('hello world hiii');
 });
 
 app.get('/home', (req, res) => {
-  res.send('hello Home by4');
+res.send('hello Home by4');
 });
 
 app.get('/new', (req, res) => {
-  res.send('New item list');
+res.send('New item list');
 });
 
 app.get('/newone', (req, res) => {
-  res.send('New item list');
+res.send('New item list');
 });
 
 const PORT = process.env.PORT || 3000;
